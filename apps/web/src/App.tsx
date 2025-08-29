8b129143984bbc5a34b9a9473a71b0ae6f6db63d
Jahid Hasan <jahidhasanimonn@gmail.com>
2025-08-30 02:24:19 +0600

    chore: add husky for running precommit hook
    - add `yarn test` and `yarn lint` to .husky/precommit file
    
    chore: add husky for running precommit hook (#1)
    
    - add `yarn test` and `yarn lint` to .husky/precommit file

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,7 @@ import { TextHoverEffect } from "./components/ui/text-hover";
 function App() {
   return (
     <div>
-      <TextHoverEffect text="Kanon" />
+      <TextHoverEffect text="Kanon Academy" />
     </div>
   );
 }
--- apps/web/src/components/ui/text-hover.tsx
+++ apps/web/src/components/ui/text-hover.tsx
@@ -91,7 +91,7 @@ export function TextHoverEffect({ text, duration }: TextHoverEffectProps) {
         textAnchor="middle"
         dominantBaseline="middle"
         strokeWidth="0.3"
-        className="fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold dark:stroke-neutral-800"
+        className="fill-transparent stroke-neutral-200 font-[helvetica] text-3xl font-bold dark:stroke-neutral-800"
         style={{ opacity: hovered ? 0.7 : 0 }}
       >
         {text}
@@ -102,7 +102,7 @@ export function TextHoverEffect({ text, duration }: TextHoverEffectProps) {
         textAnchor="middle"
         dominantBaseline="middle"
         strokeWidth="0.3"
-        className="fill-transparent stroke-neutral-200 font-[helvetica] text-7xl font-bold dark:stroke-neutral-800"
+        className="fill-transparent stroke-neutral-200 font-[helvetica] text-3xl font-bold dark:stroke-neutral-800"
         initial={{ strokeDashoffset: 1000, strokeDasharray: 1000 }}
         animate={{
           strokeDashoffset: 0,
@@ -123,7 +123,7 @@ export function TextHoverEffect({ text, duration }: TextHoverEffectProps) {
         stroke="url(#textGradient)"
         strokeWidth="0.3"
         mask="url(#textMask)"
-        className="fill-transparent font-[helvetica] text-7xl font-bold"
+        className="fill-transparent font-[helvetica] text-3xl font-bold"
       >
         {text}
       </text>
